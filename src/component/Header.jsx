ee7ee124871dda0eae2153520297f18cae033d9f
johel cueva <johel.cueva@gmail.com>
2024-11-13 17:13:00 -0500

    pull request

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,21 @@
-function Header(){
-    return(
-        <h1>Bienvenidos a la Galería Horned Beast</h1>
+import { useState } from "react";
+
+function Header() {
+
+    function idioma() {
+        alert("idioma")
+    }
+
+    const lenguaje=() =>{
+        alert("lenguaje");
+    }
+
+    return (
+        <>
+            <h1>Bienvenidos a la Galería Horned Beast</h1>
+            <button onClick={idioma}>Cambiar idioma</button>
+            <button onClick={lenguaje}>Cambiar lenguaje</button>
+        </>
     )
 }
 

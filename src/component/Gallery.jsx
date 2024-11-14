9378ff3bece4daab37b36f0a08ae7f6de0eb68f1
johel cueva <johel.cueva@gmail.com>
2024-11-13 19:33:33 -0500

    mitad del lab2

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -4,31 +4,6 @@ import data from "../data.json/";
 function Gallery() {
     return (
         <>
-            <HornedBeast
-                title={"Minotauro"}
-                imageUrl={"https://cdn.pixabay.com/photo/2024/02/15/11/04/ai-generated-8575223_1280.jpg"}
-                description={"Este es un animal mitológico"}
-            />
-            <HornedBeast
-                title={"Unicornio"}
-                imageUrl={"https://www.defondos.com/bulkupload/unicornios-wallpapers/Fantasia/Unicornios/Negro%20Unicornio_800.jpg"}
-                description={"Este es un animal mitológico"}
-            />
-            <HornedBeast
-                title={"Rinoceronte"}
-                imageUrl={"https://estaticos-cdn.prensaiberica.es/clip/f8b6643c-43a6-4235-a138-3cfa415f6588_16-9-aspect-ratio_default_0.jpg"}
-                description={"El rinoceronte negro o de labio ganchudo es una especie de mamífero perisodáctilo de la familia de los rinocerótidos"}
-            />
-            <HornedBeast
-                title={"Freezer Fase 2"}
-                imageUrl={"https://kurogami.com/med/img/productos/53/1a/sh-figuarts-freezer-segunda-forma-dragon-ball-z-1.jpg"}
-                description={"Este es un ser de Dragon Ball Z"}
-            />
-            <HornedBeast
-                title={"Cernunnos, el dios con cuernos de ciervo"}
-                imageUrl={"https://mitologiaclasica.com/wp-content/uploads/2024/04/Cernunnos-e1713437673471.webp"}
-                description={"Este es un animal mitológico"}
-            />
             {
                 data.map(beast=> (
                     <HornedBeast
--- src/component/HornedBeast.jsx
+++ src/component/HornedBeast.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 function HornedBeast(props) {
+    const [favorites, setFavorites] = useState(0);
+
+    const addFavorites = () => {
+        setFavorites(favorites + 1);
+    }
     return (
-        <> 
-            <h2>{props.title}</h2>
-            <img src={props.imageUrl} alt="" title="" />
-            <p>{props.description}</p>
+        <>
+           
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={props.imageUrl} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>
+                        {props.description}
+                    </Card.Text>
+                    <Button variant="primary">💓 Favoritos: {favorites}</Button>
+                </Card.Body>
+            </Card>
+
 
         </>
     )
